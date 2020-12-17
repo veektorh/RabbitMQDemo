ee553955df096345ca0d809de3cf755f15ca8f91
Victor Bolum <victor.bolum@cyberspace.net.ng>
2020-12-17 19:17:41 +0100

    init

---
+++ RabbitMqConsumer/RabbitMqConsumer/Program.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Configuration;
+using System.Text;
+
+namespace RabbitMqConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = ConfigurationManager.AppSettings["hostname"],
+                    Port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]),
+                    UserName = ConfigurationManager.AppSettings["username"],
+                    Password = ConfigurationManager.AppSettings["password"],
+                    //VirtualHost = ConfigurationManager.AppSettings["vhost"]
+                };
+
+                var connection = factory.CreateConnection();
+
+                binlistListener(connection);
+                testListener(connection);
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured, Make sure rabbit mq is running");
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+
+        public static void testListener(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("test", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message Received on test queue - {message}");
+            };
+
+            channel.BasicConsume("test", autoAck: true, consumer);
+
+            Console.WriteLine("Listening for messages on test queue");
+        }
+
+
+
+        public static void binlistListener(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("binlist", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message Received on bin queue - {message}");
+            };
+
+            channel.BasicConsume("binlist", autoAck: true, consumer);
+            Console.WriteLine("Listening for messages on binlist queue");
+        }
+
+    }
+}
---
+++ RabbitMqPublisher/RabbitMqPublisher/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Configuration;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory
+            {
+                HostName = ConfigurationManager.AppSettings["hostname"],
+                Port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]),
+                UserName = ConfigurationManager.AppSettings["username"],
+                Password = ConfigurationManager.AppSettings["password"],
+                //VirtualHost = ConfigurationManager.AppSettings["vhost"]
+            };
+
+            var connection = factory.CreateConnection();
+
+            publishBinlist(connection);
+            publishTest(connection);
+
+
+            Console.Read();
+        }
+
+        public static void publishBinlist(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("binlist", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var message = new { Name = "victor bolum", Scheme = "publish message" };
+            var messageString = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(messageString);
+
+            channel.BasicPublish("", "binlist", null, body);
+
+            Console.WriteLine("published binlist");
+
+        }
+
+        public static void publishTest(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("test", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var message2 = new { Name = "victor bolum", Scheme = "publish message" };
+            var messageString2 = JsonConvert.SerializeObject(message2);
+            var body2 = Encoding.UTF8.GetBytes(messageString2);
+
+            channel.BasicPublish("", "test", null, body2);
+
+            Console.WriteLine("published test");
+        }
+
+
+
+    }
+}
